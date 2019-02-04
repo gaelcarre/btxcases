edcd4db600fa1844ebfd7b3c282636f12bbada4c
CARRE Gael - m051855 <gael.carre@soprasteria.com>
2019-02-04 19:03:24 +0100

    page connexion

---
+++ page/Utilisateur_page/Connexion_page_class.php
@@ -0,0 +1,29 @@
+<?php
+	class Commande_page extends Page
+	{
+
+		public function __construct()
+		{
+			parent::__construct();
+			$this->name="Connexion";
+		}
+
+
+		public function getHead($sspage='',$param='')
+		{
+			$title = "Connexion";
+			$description = '';
+			$keywords = '';
+			$canonical = 'http://btxcases.gaelcarre.fr';
+			return parent::constructHead($title,$description,$keywords,$canonical);
+		}
+
+		public function Index($param = "")
+		{
+			$template = __PAGE_DIR__.'Utilisateur_page/template/connexion.html';
+
+			$html = $this->objSmarty->fetch($template);
+			return $html;
+		}
+	}
+?>
