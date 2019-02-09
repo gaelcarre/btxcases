6d2d5ce5426a8b0177efacb53457491556a4cd0d
CARRE Gael - m051855 <gael.carre@soprasteria.com>
2019-02-09 17:44:17 +0100

    page connexion

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 	ini_set('error_reporting', E_ALL);
 	ini_set('display_errors', 1);
 
-	
+
 	header('Content-Type: text/html; charset=UTF-8');
 	require("init.inc.php");
 	require(__UTIL_DIR__."function.php");
@@ -12,8 +12,8 @@
 	if(isset($_POST['ajaxRequest']) and $_POST['ajaxRequest'] == 1)
 	{
 		$site->requestAjax();
-		
+
 	}
 
-?>	
+?>
 <?php $site->loadContent();?>
